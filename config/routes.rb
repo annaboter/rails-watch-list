0f2f4cb18eb1aa69ae3bcb89f90bb9551e296083
Anna Bothe <annabothe0@gmail.com>
2023-11-16 16:16:12 +0100

    finished creating a view for list controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/new'
-  get 'lists/create'
+  resources :lists, only: %i[index show new create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  get 'lists',     to: 'lists#index'
-  get 'lists/:id', to: 'lists#show'
-  get 'lists/new', to: 'lists#new'
-  post 'lists',    to: 'lists#create'
   # Defines the root path route ("/")
   # root "posts#index"
 end
